9aa5827f9e564b83b97a7e45b42c879b1384ced8
Marrio Hinkle <kalunga212@gmail.com>
2018-11-01 18:48:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var name = "Joe"
-var height = "74"
+var name = "Joe";
+var height = "74";
 
 
 // Don't worry about this
@@ -10,3 +10,4 @@ module.exports = { name, height
 //} else {
 //  console.log ("Expected the name to be Susan")
 //
+
